71b76026dbadb2768fed5364b35cc09077f53ef6
Rimantas <belauzas@yahoo.com>
2022-01-15 04:18:12 +0200

    Support relative path specified for middleware

--- index.js
+++ index.js
@@ -185,8 +185,11 @@ DeadServer.start = function (options) {
 			var ext = path.extname(mw).toLocaleLowerCase();
 			if (ext !== ".js") {
 				mw = require(path.join(__dirname, "middleware", mw + ".js"));
-			} else {
+			}
+			else if (path.isAbsolute(mw)) {
 				mw = require(mw);
+			} else {
+				mw = require(path.join(process.cwd(), mw));
 			}
 		}
 		app.use(mw);
